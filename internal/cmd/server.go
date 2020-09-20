c2958369c2d3a789b9282524d8d86f031b5883ff
hwipl <33433250+hwipl@users.noreply.github.com>
2020-09-20 13:04:32 +0200

    add command "bye"
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"net"
+	"strings"
 )
 
 // server stores server information
@@ -12,11 +13,20 @@ type server struct {
 	address  string
 	listener net.Listener
 	conn     net.Conn
+
+	// is client active?
+	clientActive bool
 }
 
 // handleCommand handles a command receviced from the client
 func (s *server) handleCommand(cmd string) {
 	log.Println("client:", cmd)
+
+	parts := strings.Split(cmd, " ")
+	switch parts[0] {
+	case "bye":
+		s.clientActive = false
+	}
 }
 
 // handleClient handles a single client connection
@@ -24,9 +34,10 @@ func (s *server) handleClient() {
 	defer s.conn.Close()
 	log.Println("New client connection", s.conn.RemoteAddr())
 
+	s.clientActive = true
 	r := bufio.NewReader(s.conn)
 	c := ""
-	for {
+	for s.clientActive {
 		// read a cmd line from the client
 		cmd, err := r.ReadString('\n')
 		if err != nil {
