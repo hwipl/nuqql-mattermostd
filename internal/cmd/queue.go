3dfd9ac1f0dd418abbab5f6210673e77e61a7838
hwipl <33433250+hwipl@users.noreply.github.com>
2020-09-23 16:54:46 +0200

    add internal/cmd/queue.go
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/queue.go
@@ -0,0 +1,101 @@
+package cmd
+
+import (
+	"bufio"
+	"log"
+	"net"
+)
+
+var (
+	clientQueue = newQueue()
+)
+
+// queue stores messages for the client
+type queue struct {
+	queue    []string
+	client   net.Conn
+	clients  chan net.Conn
+	messages chan string
+}
+
+// sendToClient sends the contents of the message queue to the client
+func (q *queue) sendToClient() {
+	w := bufio.NewWriter(q.client)
+	for len(q.queue) > 0 {
+		msg := q.queue[0]
+		n, err := w.WriteString(msg)
+		if n < len(msg) || err != nil {
+			q.client.Close()
+			q.client = nil
+			log.Println(err)
+			break
+		}
+		if err := w.Flush(); err != nil {
+			q.client.Close()
+			q.client = nil
+			log.Println(err)
+			break
+		}
+		q.queue = q.queue[1:]
+	}
+}
+
+// run starts the main loop of the queue
+func (q *queue) run() {
+	for {
+		select {
+		case c, more := <-q.clients:
+			// handle client (de)registration
+			if !more {
+				q.clients = nil
+			}
+			q.client = c
+
+			if q.client != nil {
+				// new client, send all queued messages to
+				// the client
+				q.sendToClient()
+			}
+
+		case m, more := <-q.messages:
+			// handle message for client
+			if !more {
+				q.messages = nil
+			}
+
+			// append message to the message queue
+			q.queue = append(q.queue, m)
+			if q.client == nil {
+				continue
+			}
+
+			// client is active, send all queued messages to client
+			q.sendToClient()
+		}
+
+		// all channels closed, stop here
+		if q.clients == nil && q.messages == nil {
+			return
+		}
+	}
+}
+
+// queue sends msg to the (future) client via the queue
+func (q *queue) send(msg string) {
+	q.messages <- msg
+}
+
+// setClient sets conn as the client
+func (q *queue) setClient(conn net.Conn) {
+	q.clients <- conn
+}
+
+// newQueue creates a new queue
+func newQueue() *queue {
+	q := queue{
+		clients:  make(chan net.Conn),
+		messages: make(chan string),
+	}
+	go q.run()
+	return &q
+}
