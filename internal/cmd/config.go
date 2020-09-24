189295a427fd02b9d658b5697741afb70cfd6dfe
hwipl <33433250+hwipl@users.noreply.github.com>
2020-09-24 16:06:05 +0200

    store dir with trailing slash in config
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -14,7 +14,7 @@ var (
 type config struct {
 	// name is the name of this configuration, e.g., "nuqql-mattermostd"
 	name string
-	// dir is the working directory
+	// dir is the working directory including trailing "/"
 	dir string
 }
 
@@ -25,7 +25,7 @@ func newConfig(name string) *config {
 	if err != nil {
 		log.Fatal(err)
 	}
-	dir := filepath.FromSlash(confDir + "/" + name)
+	dir := filepath.FromSlash(confDir + "/" + name + "/")
 
 	// create and return config
 	c := config{
