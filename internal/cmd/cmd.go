3bf391f30cd8dc0d8e34715919b4d506f3a4bb6f
hwipl <33433250+hwipl@users.noreply.github.com>
2020-09-26 21:17:58 +0200

    add helper initDirectory()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -5,13 +5,19 @@ import (
 	"os"
 )
 
-// Run is the main entry point
-func Run() {
-	// make sure working directory exists
+// initDirectory makes sure the working directory exists
+func initDirectory() {
 	err := os.MkdirAll(conf.dir, 0700)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+// Run is the main entry point
+func Run() {
+	// make sure working directory exists
+	initDirectory()
 
 	// start accounts and client connections
 	startAccounts()
