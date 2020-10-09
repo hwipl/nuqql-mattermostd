cf7ea8b9ca81c2d054278349dda23296d6c485e0
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-09 15:08:20 +0200

    add history to queue
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/queue.go
+++ internal/cmd/queue.go
@@ -16,6 +16,10 @@ type queue struct {
 	client   net.Conn
 	clients  chan net.Conn
 	messages chan string
+
+	noHistory bool
+	history   []string
+	histReqs  chan struct{}
 }
 
 // sendToClient sends the contents of the message queue to the client
@@ -37,6 +41,26 @@ func (q *queue) sendToClient() {
 			break
 		}
 		q.queue = q.queue[1:]
+	}
+}
+
+// sendHistoryToClient sends the contents of the message history to the client
+func (q *queue) sendHistoryToClient() {
+	w := bufio.NewWriter(q.client)
+	for _, msg := range q.history {
+		n, err := w.WriteString(msg)
+		if n < len(msg) || err != nil {
+			q.client.Close()
+			q.client = nil
+			log.Println(err)
+			break
+		}
+		if err := w.Flush(); err != nil {
+			q.client.Close()
+			q.client = nil
+			log.Println(err)
+			break
+		}
 	}
 }
 
@@ -65,19 +89,42 @@ func (q *queue) run() {
 
 			// append message to the message queue
 			q.queue = append(q.queue, m)
+
+			// add message to the history
+			if q.noHistory {
+				continue
+			}
+			q.history = append(q.history, m)
+
+			// send all queued messages to client if it's active
 			if q.client == nil {
 				continue
 			}
-
-			// client is active, send all queued messages to client
 			q.sendToClient()
+
+		case _, more := <-q.histReqs:
+			// handle get history request from client
+			if !more {
+				q.histReqs = nil
+			}
+
+			// send all messages in history to client
+			if q.client == nil {
+				continue
+			}
+			q.sendHistoryToClient()
 		}
 
 		// all channels closed, stop here
-		if q.clients == nil && q.messages == nil {
+		if q.clients == nil && q.messages == nil && q.histReqs == nil {
 			return
 		}
 	}
+}
+
+// getHistory requests the complete message history from the queue
+func (q *queue) getHistory() {
+	q.histReqs <- struct{}{}
 }
 
 // queue sends msg to the (future) client via the queue
@@ -95,6 +142,7 @@ func newQueue() *queue {
 	q := queue{
 		clients:  make(chan net.Conn),
 		messages: make(chan string),
+		histReqs: make(chan struct{}),
 	}
 	go q.run()
 	return &q
