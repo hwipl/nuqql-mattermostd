4a6b553df109f479a79d84ed9057440754d581fd
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-17 21:03:05 +0200

    add test for getFreeAccountID()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/account_test.go
+++ internal/cmd/account_test.go
@@ -24,3 +24,47 @@ func TestSplitAccountUser(t *testing.T) {
 		t.Errorf("got %s, wanted %s", got, want)
 	}
 }
+
+func TestGetFreeAccountID(t *testing.T) {
+	accounts = make(map[int]*account)
+	defer func() {
+		// cleanup
+		accounts = make(map[int]*account)
+	}()
+
+	// test empty
+	want := 0
+	got := getFreeAccountID()
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+	// test filled
+	accounts[0] = &account{ID: 0}
+	accounts[1] = &account{ID: 1}
+	accounts[2] = &account{ID: 2}
+
+	want = 3
+	got = getFreeAccountID()
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+	// test filled (see above) with gap
+	accounts[4] = &account{ID: 4}
+
+	want = 3
+	got = getFreeAccountID()
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+
+	// test with filled gap
+	accounts[3] = &account{ID: 3}
+
+	want = 5
+	got = getFreeAccountID()
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
