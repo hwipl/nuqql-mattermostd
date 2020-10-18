01f85de067b04980075fcb1b9fe213a67ca560b0
hwipl <33433250+hwipl@users.noreply.github.com>
2020-10-18 17:01:27 +0200

    add test for getAccount()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/account_test.go
+++ internal/cmd/account_test.go
@@ -37,6 +37,46 @@ func TestAccountStop(t *testing.T) {
 	a.stop()
 }
 
+func TestGetAccount(t *testing.T) {
+	var want, got *account
+	accounts = make(map[int]*account)
+	defer func() {
+		// cleanup
+		accounts = make(map[int]*account)
+	}()
+
+	// add entries to accounts
+	accounts[0] = &account{ID: 0}
+	accounts[1] = &account{ID: 1}
+	accounts[2] = &account{ID: 2}
+
+	// test existing entries
+	want = accounts[0]
+	got = getAccount(0)
+	if got != want {
+		t.Errorf("got %p, wanted %p", got, want)
+	}
+
+	want = accounts[1]
+	got = getAccount(1)
+	if got != want {
+		t.Errorf("got %p, wanted %p", got, want)
+	}
+
+	want = accounts[2]
+	got = getAccount(2)
+	if got != want {
+		t.Errorf("got %p, wanted %p", got, want)
+	}
+
+	// test non existing entry
+	want = nil
+	got = getAccount(3)
+	if got != want {
+		t.Errorf("got %p, wanted %p", got, want)
+	}
+}
+
 func TestGetFreeAccountID(t *testing.T) {
 	accounts = make(map[int]*account)
 	defer func() {
