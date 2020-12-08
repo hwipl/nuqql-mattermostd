1f2804e257f7b483d823cb8c74dab88cbe5522af
hwipl <33433250+hwipl@users.noreply.github.com>
2020-12-08 15:27:40 +0100

    add type channels
    Add the type channels that stores information about joined channels. It
    stores the last know post ids of every joined channel and reads/writes
    them from/to a file on disk.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/cmd/channels.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// channels stores information about joined channels
+type channels struct {
+	postIDs  map[string]string
+	fileName string
+}
+
+// getPostID returns the last known postID in the channel identified by chanID
+func (c *channels) getPostID(chanID string) string {
+	return c.postIDs[chanID]
+}
+
+// updatePostID updates the last known postID in the channel identified by
+// its chanID
+func (c *channels) updatePostID(chanID, postID string) {
+	c.postIDs[chanID] = postID
+	c.writeToFile()
+}
+
+// readFromFile reads channels from file
+func (c *channels) readFromFile() {
+	file := filepath.Join(conf.Dir, c.fileName)
+
+	// open file for reading
+	f, err := os.Open(file)
+	if err != nil {
+		logError(err)
+		return
+	}
+
+	// read accounts from file
+	dec := json.NewDecoder(f)
+	for {
+		err := dec.Decode(&c.postIDs)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			logFatal(err)
+		}
+	}
+}
+
+// writeToFile writes channels to file
+func (c *channels) writeToFile() {
+	file := filepath.Join(conf.Dir, c.fileName)
+
+	// open file for writing
+	f, err := os.Create(file)
+	if err != nil {
+		logFatal(err)
+	}
+
+	// make sure file is only readable and writable by the current user
+	err = os.Chmod(file, 0600)
+	if err != nil {
+		logFatal(err)
+	}
+
+	// write accounts to file
+	enc := json.NewEncoder(f)
+	err = enc.Encode(c.postIDs)
+	if err != nil {
+		logFatal(err)
+	}
+}
+
+func newChannels(accountID int) *channels {
+	c := channels{
+		postIDs:  make(map[string]string),
+		fileName: fmt.Sprintf("channels%d.json", accountID),
+	}
+	c.readFromFile()
+	return &c
+}
