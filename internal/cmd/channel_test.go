031fbe3c9f8d73846bc0cfa1ce9623e2f87a0890
hwipl <33433250+hwipl@users.noreply.github.com>
2020-12-09 15:59:45 +0100

    add test for getting the latest post id of a channel
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/channel_test.go
+++ internal/cmd/channel_test.go
@@ -4,6 +4,31 @@ import (
 	"testing"
 )
 
+func TestChannelsGetPostID(t *testing.T) {
+	// configure working directory
+	dir := createTestWorkDir()
+	defer removeTestWorkDir(dir)
+	conf.Dir = dir
+
+	// create channels
+	c := newChannels(0)
+
+	// get non-existent channel/post id
+	want := ""
+	got := c.getPostID("channelID")
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+
+	// get existent channel/post id
+	c.updatePostID("channelID", "postID")
+	want = "postID"
+	got = c.getPostID("channelID")
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
 func TestChannelsReadFromFile(t *testing.T) {
 	// configure working directory
 	dir := createTestWorkDir()
